a437ed8e52ba5e7745ec58df3818780108983e9c
Carlos Dominguez <dominguerilla@gmail.com>
2019-06-12 17:39:55 -0400

    init

---
+++ Assets/StarRotator.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarRotator : MonoBehaviour
+{
+    [SerializeField]
+    float speed = 3.0f;
+
+    void Update()
+    {
+        transform.Rotate(Vector3.up, speed * Time.deltaTime, Space.World);    
+    }
+}
