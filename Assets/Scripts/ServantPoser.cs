fd734e3d8f3985561cfb5527d04c36dce458b1a1
Carlos Dominguez <dominguerilla@gmail.com>
2019-06-21 18:25:14 -0400

    Enable basic pose switching

--- Assets/Scripts/ServantPoser.cs
+++ Assets/Scripts/ServantPoser.cs
@@ -1,27 +1,53 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
+/// <summary>
+/// On every frame, 'poses' the servant according to the currently active posing function.
+/// Switches the pose function every time the player launches.
+/// </summary>
 public class ServantPoser : MonoBehaviour
 {
     [SerializeField] CursorOrbitter orbitter;
     public GameObject playerMesh;
 
+    PlayerController player;
     GameObject cursor;
+    UnityAction poseFunction;
+
+    bool poseFlag = false;
 
     private void Start() {
+        player = GameObject.FindObjectOfType<PlayerController>();
+        SetNextPoseFunction();
+        player.onLaunch.AddListener(SetNextPoseFunction);
         cursor = orbitter.GetCursor();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Point(cursor.transform.position);
+        poseFunction.Invoke();
     }
 
-    void Point(Vector3 referencePoint){
-        Vector3 direction = referencePoint - playerMesh.transform.position;        
+    void SetNextPoseFunction(){
+        
+        if(!poseFlag){
+            poseFunction = PointTowardsCursor;
+        }else{
+            playerMesh.transform.right = cursor.transform.position - playerMesh.transform.position;
+            poseFunction = RotateAroundYAxis;
+        }
+        poseFlag = !poseFlag;
+    }
+
+    void PointTowardsCursor(){
+        Vector3 direction = cursor.transform.position - playerMesh.transform.position;        
         playerMesh.transform.rotation = Quaternion.FromToRotation(Vector3.up, direction);
     }
 
+    void RotateAroundYAxis(){
+        playerMesh.transform.Rotate(transform.up, 300 * Time.deltaTime, Space.Self);
+    }
 }
